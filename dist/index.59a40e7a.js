c6f0ef8b6e04b44ca63d7b51765f2c307dc0f97d
Alfonso J <alfonsojfp@gmail.com>
2024-11-13 21:56:39 +0100

    Cambios estructura

--- dist/index.59a40e7a.js
+++ dist/index.59a40e7a.js
@@ -585,6 +585,39 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"8lRBv":[function(require,module,exports) {
 console.log("Iniciando carga de contenido");
+document.addEventListener("DOMContentLoaded", ()=>{
+    const links = document.querySelectorAll(".menu-link");
+    links.forEach((link)=>{
+        link.addEventListener("click", (event)=>{
+            console.log("click en", link);
+            event.preventDefault();
+            links.forEach((link)=>{
+                link.classList.remove("active");
+            });
+            const contentUrl = event.target.getAttribute("href");
+            const href = link.getAttribute("href");
+            if (contentUrl == href) link.classList.add("active");
+        });
+    });
+}); /*
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.load-content');
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const contentUrl = event.target.getAttribute('data-content');
+
+            fetch(contentUrl)
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('app').innerHTML = data;
+                })
+                .catch(error => console.error('Error cargando el contenido:', error));
+        });
+    });
+});
+*/ 
 
 },{}]},["aP7aF","8lRBv"], "8lRBv", "parcelRequire3c64")
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,43 @@
 console.log('Iniciando carga de contenido');
 
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.menu-link');
+    
+    links.forEach(link => {
+        
+                
+        link.addEventListener('click', event => {
+            console.log("click en", link);
+            event.preventDefault();
+            
+            links.forEach((link) => {link.classList.remove('active');});
+
+            const contentUrl = event.target.getAttribute('href');
+            const href = link.getAttribute('href');
+            if(contentUrl == href){
+                link.classList.add('active');
+            }
+            
+        });
+    });
+});
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.load-content');
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const contentUrl = event.target.getAttribute('data-content');
+
+            fetch(contentUrl)
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('app').innerHTML = data;
+                })
+                .catch(error => console.error('Error cargando el contenido:', error));
+        });
+    });
+});
+*/
